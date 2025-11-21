d6e711784e658ffe9e9f41686134fad947b763e2
Sabrina D. <sabrinadorzin103@gmail.com>
2025-11-21 08:56:46 -0500

    insert-01

---
+++ insert-01.sql
@@ -0,0 +1,4 @@
+use northwind;
+
+INSERT INTO suppliers (SupplierId, CompanyName, ContactName, Address, City, PostalCode, Country, Phone)
+VALUES (1200, 'TechMeller', 'Sabrina Shonk', '114 Street Lane', 'New York', '10115', 'Usa', '803-599-1445');
